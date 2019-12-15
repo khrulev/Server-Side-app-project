5f97716b00d428ba44fdff90dade08fde772b093
Ivan Khrulev <subz07@bk.ru>
2019-12-15 15:42:47 +0500

    HTTPS

--- app.js
+++ app.js
@@ -28,6 +28,15 @@ connect.then((db) => {
     console.log("Connected correctly to server");
 }, (err) => { console.log(err); });
 
+// Secure traffic only
+app.all('*', (req, res, next) => {
+  if (req.secure) {
+    return next();
+  }
+  else {
+    res.redirect(307, 'https://' + req.hostname + ':' + app.get('secPort') + req.url);
+  }
+});
 
 // view engine setup
 app.use(session({
--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,22 @@
 var express = require('express');
-var router = express.Router();
-
 const bodyParser = require('body-parser');
 var User = require('../models/user');
-
+var router = express.Router();
 var passport = require('passport');
 var authenticate = require('../authenticate');
-
 router.use(bodyParser.json());
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.route('/')
+.get(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+  User.find({})
+      .then((user) => {
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'application/json');
+          res.json(user);
+      }, (err) => next(err))
+      .catch((err) => next(err));
+})
 
 router.post('/signup', (req, res, next) => {
   User.register(new User({username: req.body.username}), 
@@ -45,13 +49,11 @@ router.post('/signup', (req, res, next) => {
 });
 
 router.post('/login', passport.authenticate('local'), (req, res) => {
-
   var token = authenticate.getToken({_id: req.user._id});
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
-  res.json({success: true, token: token, status: 'You are successfully logged in!'});
+  res.json({admin: req.user.admin,success: true, token: token, status: 'You are successfully logged in!'});
 });
-
 
 router.get('/logout', (req, res) => {
   if (req.session) {
