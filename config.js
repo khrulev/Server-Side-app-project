ce2fd639a7482c3f7810b7249a32ca3a190fffb3
Ivan Khrulev <subz07@bk.ru>
2019-12-15 23:36:10 +0500

    Passport Facebook 1

--- config.js
+++ config.js
@@ -1,4 +1,7 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
-}
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: 'ID2473861776204528',
+        clientSecret: '89ef9d704f2f1be94ce021e5220431ee'
+}}
